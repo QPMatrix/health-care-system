421e84fc2794ec67b177b09f0b67bdede7d53bcc
QPM <admin@QuantumPinnacleMatrix.tech>
2024-07-06 15:07:19 +0300

    done with home screen

---
+++ components/forms/patient-form.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Form } from "@/components/ui/form";
+import CFormField from "@/components/custom/form-field";
+import SubmitBtn from "@/components/custom/submit-btn";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+export enum FormFieldType {
+  INPUT = "input",
+  CHECKBOX = "checkbox",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneinput",
+  DATE_PICKER = "datepicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
+
+const PatientForm = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    try {
+      //   const userData = {
+      //     name,
+      //     email,
+      //     phone,
+      //   };
+      //   const user = await createUser(userData);
+      //   if (user) router.push(`/patients/${user.$id}/register`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+          <CFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="name"
+            label="Full name"
+            placeholder="Hasan Diab"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="User"
+          />
+          <CFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="example@qpmatrix.tech"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="Email"
+          />
+          <CFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone Number"
+            placeholder="(972) 123-4567"
+          />
+          <SubmitBtn isLoading={form.formState.isLoading}>
+            Get Started
+          </SubmitBtn>
+        </section>
+      </form>
+    </Form>
+  );
+};
+
+export default PatientForm;
